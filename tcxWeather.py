22288ca3f84542bf9d04922d7625709ab9c153d6
William Jackson <william.jackson@strath.ac.uk>
2017-03-17 19:29:52 +0000

    weather inherits from tcx

--- tcxWeather.py
+++ tcxWeather.py
@@ -13,7 +13,8 @@ from datetime import datetime, timedelta, date, time
 
 from pytz import timezone
 
-class tcxWeather:
+
+class TcxRide:
     def __init__(self, xmlfile='demoRoute.tcx'):
         self.raw = tcxparser.TCXParser(xmlfile)
         self.stravaTime = self.raw.time_values()
@@ -25,10 +26,9 @@ class tcxWeather:
         self.__bearing__()
         self.tZ = timezone('Europe/London')
 
-
     def __bearing__(self):
         self.bearing = list()
-        self.bearing.append(0) #first bearing 0
+        self.bearing.append(0) # first bearing 0
         phi = list()
         lambd = list()
         for x in self.latitude:
@@ -40,10 +40,9 @@ class tcxWeather:
             x = np.cos(phi[x-1])*np.sin(phi[x]) - np.sin(phi[x-1])*np.cos(phi[x])*np.cos(lambd[x]-lambd[x-1])
             self.bearing.append(np.degrees(np.arctan2(y, x))%360)
 
-
-    def __bearingdec__(self): #fix to one function for both bearings
+    def __bearingdec__(self): # fix to one function for both bearings
         self.bear = list()
-        self.bear.append(0) #first bearing 0
+        self.bear.append(0) # first bearing 0
         phi = list()
         lambd = list()
         for x in self.lat:
@@ -53,8 +52,7 @@ class tcxWeather:
         for x in range(1, len(self.lat)):
             y = np.sin(lambd[x]-lambd[x-1]) * np.cos(phi[x])
             x = np.cos(phi[x-1])*np.sin(phi[x]) - np.sin(phi[x-1])*np.cos(phi[x])*np.cos(lambd[x]-lambd[x-1])
-            self.bear.append(np.degrees(np.arctan2(y, x))%360) #0-360 instead of -180:180
-
+            self.bear.append(np.degrees(np.arctan2(y, x))%360) # 0-360 instead of -180:180
 
     def speed(self, **kwargs):
         mps_mph = 2.23694
@@ -73,21 +71,21 @@ class tcxWeather:
             self.mph = self.mps*mps_mph
         self.__time__()
 
-
     def decimate(self, **kwargs):
         if 'Distance' in kwargs:
             distance = kwargs['Distance']
-            #points not constant this is average
+            # points not constant this is average
             numPoints = self.distanceTotal/distance
-        if 'Points' in kwargs:
+        elif 'Points' in kwargs:
             numPoints = kwargs['Points']
-        if 'Time' in kwargs:
+        elif 'Time' in kwargs:
             if hasattr(self, 'mps'):
                 numPoints = self.totalTime/kwargs['Time']
             else:
                 raise Exception('no speed defined use x.Speed(mps =y | kph =z | mph =w)')
         else:
-            numPoints = 10
+            raise Exception('Define decimation in terms of Distance= (m)| Points = (n)| Time = (s)')
+
         numPoints = np.floor(numPoints).astype(int)
         print('Decimating to {0} Points'.format(numPoints))
         ind = np.linspace(0, (self.length-1), numPoints, endpoint=True, retstep=False, dtype=None)
@@ -103,7 +101,6 @@ class tcxWeather:
         self.__bearingdec__()
         self.__timeDec__()
 
-
     def __timeDec__(self):
         self.time = list()
         timeSectoAdd = 0
@@ -116,7 +113,6 @@ class tcxWeather:
             self.time.append(combined)
             
 
-
     def __time__(self):
         self.timeSeconds = list()
         self.timeSeconds.append(0)
@@ -128,7 +124,29 @@ class tcxWeather:
         self.totalTime = time
 
 
+
+    def setRideStartTime(self, **kwargs): #TODO (Check is in range of forecast)
+        if 'date' in kwargs:
+            date = datetime.strptime(kwargs["date"], "%d/%m").date()
+            date = date.replace(year=datetime.today().year)
+        else:
+            date = datetime.today().date()
+        if 'time' in kwargs:
+            time = datetime.strptime(kwargs["time"], "%H:%M").time()
+        self.rideStartTime = datetime.combine(date, time)
+        self.rideStartTime = self.tZ.localize(self.rideStartTime)
+
+
+class RideWeather(TcxRide):
+    """
+    Ride weather class
+    """
+
+    def __init__(self):
+        TcxRide.__init__(self,xmlfile='demoRoute.tcx')
+
     def getWeatherData(self, apikey='none', units='si', writeF=False):
+        urlprov = 'https://api.darksky.net/forecast/'
         if hasattr(self, 'weatherData'):
             raise Exception('Data already exists')
         self.weatherData = list()
@@ -137,17 +155,17 @@ class tcxWeather:
         else:
             raise Exception('Data not decimated not making API call')
         for x in range(0, self.len):
-            url = 'https://api.darksky.net/forecast/{0}/{1},{2}?exclude=daily,alerts,flags?units={3}'.format(apikey, self.lat[x], self.lon[x], units)
-            data = (requests.get(url).content)
+            url = '{0}/{1},{2}?exclude=daily,alerts,flags?units={3}'.format(urlprov, self.lat[x], self.lon[x], units)
+            data = requests.get(url).content
             if writeF:
                 if not os.path.exists('weatherData'):
                     os.makedirs('weatherData')
                 file = open('weatherData/weatherdataDemoTCX{0}.json'.format(x), 'wb')
                 file.write(data)
-                file.close
+                file.close()
             self.weatherData.append(json.loads(data))
         print('Gathered weather data')
-        print(self.time)
+
 
     def loadExistingData(self, location):
         if hasattr(self, 'weatherData'):
@@ -159,12 +177,10 @@ class tcxWeather:
             with open(filename) as data_file:
                 self.weatherData.append(json.load(data_file))
 
-
     def clearWeatherData(self):
         del self.weatherData
 
-
-    def getForecast(self): #potentially make this its own class inheriting from tcxweather
+    def getForecast(self):  # potentially make this its own class inheriting from tcxweather
         self.precipIntensity = list()
         self.windBearing = list()
         self.relWindBear = list()
@@ -189,13 +205,15 @@ class tcxWeather:
         self.timeMin = list()
         self.timeHourTime = list()
         for x in range(0, self.len):
-            #self..append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]][""])
-            self.forecastTime.append(self.tZ.localize(datetime.fromtimestamp(int(self.weatherData[x]["currently"]["time"]))))
-            self.deltaTime.append((self.time[x]-self.forecastTime[x]))
-            self.timeMin.append((np.round((self.deltaTime[x]/timedelta(minutes=1)))).astype(int))
-            self.timeHr.append((np.round((self.deltaTime[x]/timedelta(hours=1)))).astype(int))
+            # self..append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]][""])
+            self.forecastTime.append(
+                self.tZ.localize(datetime.fromtimestamp(int(self.weatherData[x]["currently"]["time"]))))
+            self.deltaTime.append((self.time[x] - self.forecastTime[x]))
+            self.timeMin.append((np.round((self.deltaTime[x] / timedelta(minutes=1)))).astype(int))
+            self.timeHr.append((np.round((self.deltaTime[x] / timedelta(hours=1)))).astype(int))
             self.windBearing.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["windBearing"])
-            self.apparentTemperature.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["apparentTemperature"])
+            self.apparentTemperature.append(
+                self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["apparentTemperature"])
             self.cloudCover.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["cloudCover"])
             self.dewPoint.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["dewPoint"])
             self.humidity.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["humidity"])
@@ -208,30 +226,25 @@ class tcxWeather:
             self.visibility.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["visibility"])
             self.windBearing.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["windBearing"])
             self.windSpeed.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["windSpeed"])
-            self.relWindBear.append((self.windBearing[x]-self.bear[x])%360)
+            self.relWindBear.append((self.windBearing[x] - self.bear[x]) % 360)
             if (self.timeMin[x]) < 60:
-                self.precipProbability.append(self.weatherData[x]["minutely"]["data"][self.timeMin[x]]["precipProbability"])
-                self.precipIntensity.append(self.weatherData[x]["minutely"]["data"][self.timeMin[x]]["precipIntensity"])
+                self.precipProbability.append(
+                    self.weatherData[x]["minutely"]["data"][self.timeMin[x]]["precipProbability"])
+                self.precipIntensity.append(
+                    self.weatherData[x]["minutely"]["data"][self.timeMin[x]]["precipIntensity"])
                 if self.precipIntensity[x] > 0:
                     self.precipType.append(self.weatherData[x]["minutely"]["data"][self.timeMin[x]]["precipType"])
                 else:
                     self.precipType.append("None")
             else:
-                self.precipIntensity.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["precipIntensity"])
-                self.precipProbability.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["precipProbability"])
+                self.precipIntensity.append(
+                    self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["precipIntensity"])
+                self.precipProbability.append(
+                    self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["precipProbability"])
                 if self.precipIntensity[x] > 0:
                     self.precipType.append(self.weatherData[x]["hourly"]["data"][self.timeHr[x]]["precipType"])
                 else:
                     self.precipType.append("None")
 
 
-    def setRideStartTime(self, **kwargs): #TODO (Check is in range of forecast)
-        if 'date' in kwargs:
-            date = datetime.strptime(kwargs["date"], "%d/%m").date()
-            date = date.replace(year=datetime.today().year)
-        else:
-            date = datetime.today().date()
-        if 'time' in kwargs:
-            time = datetime.strptime(kwargs["time"], "%H:%M").time()
-        self.rideStartTime = datetime.combine(date, time)
-        self.rideStartTime = self.tZ.localize(self.rideStartTime)
+
