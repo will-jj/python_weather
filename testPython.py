444127af26719049d022f7b39de4bdf9d467f4bd
William Jackson <william.jackson@strath.ac.uk>
2017-03-18 17:07:16 +0000

    Python testing

--- testPython.py
+++ testPython.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 import tcxWeather
-
+from matplotlib import pyplot as plot
 
 steve=tcxWeather.tcxWeather()
 steve.speed(kph=25)
@@ -8,8 +8,14 @@ steve.setRideStartTime(date ="12/03",time = "15:32")
 steve.decimate(Time=1000)
 steve.loadExistingData('weatherData/weatherdataDemoTCX')
 steve.getForecast()
+"""
 pprint(steve.precipIntensity)
 print('Wind bearing:',steve.windBearing[3])
 print('Rider bearing:',steve.bear[3])
 print('Rel bearing:',steve.relWindBear[3])
 ##steve.getWeatherData(apikey='XXXXXXXXXX',units='si',writeF=True)
+plot.Line2D(steve.timeMin,steve.relWindBear)
+plot.show()
+pprint(steve.timeMin)
+pprint(steve.relWindBear)
+"""
