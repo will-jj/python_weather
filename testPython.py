60c8d76587d4cc95c783cf5fe4bcbc4c352a9198
William Jackson <william.jackson@strath.ac.uk>
2017-03-25 17:22:39 +0000

    Modified test script to reflect changes in class

--- testPython.py
+++ testPython.py
@@ -1,22 +1,24 @@
-import tcxWeather
+from tcxWeather import  RideWeather as RdWth
 from pprint import pprint
 from matplotlib import pyplot as plt
 import numpy as np
-
-
-
 plt.close("all")
-steve= tcxWeather.RideWeather()
+steve= RdWth(loadPrev='weatherTAKCROW2/CrowTakPickle.pkl')
+# steve = RdWth(xmlfile='TakCrowPopularityCreated.tcx')print(steve.length)
 steve.speed(kph=25)
-steve.setRideStartTime(date ="12/03", time = "15:32")
-steve.decimate(Time=1000)
-steve.loadExistingData('weatherData/weatherdataDemoTCX')
-steve.getForecast()
+steve.setRideStartTime(date ="25/03", time = "20:00")
+steve.decimate(Points=10)
+
+# steve.getWeatherData('apiremoved', fileDirectory='weatherTAKCROW2', fileName='weatherdataDemoTCX',units='si')
+# steve.loadExistingData('weatherTAKCROW2/weatherdataDemoTCX')
+# steve.getForecast(fileDirectory='weatherTAKCROW2', fileName='CrowTakPickle')
+
+'''
 pprint(steve.precipIntensity)
 print('Wind bearing:' ,steve.windBearing[3])
 print('Rider bearing:' ,steve.bear[3])
 print('Rel bearing:',steve.relWindBear[3])
-##steve.getWeatherData(apikey='XXXXXXXXXX',units='si',writeF=True)
+'''
 
 myBear = list()
 # -180:180 plots nicer
@@ -25,11 +27,39 @@ for x in steve.relWindBear:
 fig = plt.figure()
 ax = fig.gca()
 
-# ax.set_xticks(numpy.arange(0,1,0.1))
-# ax.set_yticks(numpy.arange(0,1.,0.1))
-plt.plot(steve.timeMin,myBear,'r--')
-plt.xlabel('Delta Weather Time [mins]')
+
+plt.stem(steve.dist,steve.relWindBear)
+plt.xlabel('Distance [m]')
 plt.ylabel('Relative Wind Bearing [degrees]')
 plt.title('Relative Wind Bearing')
+plt.grid()
+
+
+fig2 = plt.figure()
+ax2 = fig.gca()
+plt.plot(steve.dist,steve.windSpeed)
+plt.xlabel('Distance [m]')
+plt.ylabel('Wind Speed [kph]')
+plt.title('Wind Speed')
+plt.grid()
+
+
+
+fig3 = plt.figure()
+ax3 = fig.gca()
+plt.plot(steve.dist,steve.precipProbability)
+plt.xlabel('Distance [m]')
+plt.ylabel('Probability')
+plt.title('Precipitation Probability')
+plt.grid()
+
+
+fig4 = plt.figure()
+ax4 = fig.gca()
+plt4,plt5 = plt.plot(steve.dist, steve.apparentTemperature, steve.dist, steve.temperature)
+plt.xlabel('Distance [m]')
+plt.ylabel(r"Temperature ""$^oC$")
+plt.title('Temperature')
+plt.legend([plt4,plt5], ['Apparent Temperature', 'Actual Temperature'])
 plt.grid()
 plt.show()
